9f6283c68841888c3bcae659b3942ab54d4c6974
Joel Ribeiro <joelribeirod2201@gmail.com>
2024-09-13 13:51:26 -0400

    carrossel finalizado

---
+++ Tentativas/Tentativa05/codigo.js
@@ -0,0 +1,49 @@
+const slider = document.querySelectorAll('.on')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+
+let imgAtual = 0
+
+function addicionar(){
+    slider.forEach(item => item.classList.add('imgs'))//1;  o problema tava nessas 2 linhas
+}
+
+function remover(){
+    slider[imgAtual].classList.remove('imgs')//2; 
+}
+//esse carrosel, só coloca a classe imgs(classe que faz a imagem sumir) em uma unica imagem por vez, ou seja
+//se a imagem 0 desaparece, a imagem 2 é mostrada
+//se a imagem 1 desaparece, a imagem 2 é mostrada
+//se a imagem 2 desaparece, a imagem 1 é mostrada
+//de forma que, a imagem 0 em nenhum dos casos aparece
+
+//problema resolvido: a logica antes era; não mostrar uma imagem em especifico
+//agora a logica esta em; mostrar uma imagem em especifico
+function prox(){
+    addicionar()
+    let teste
+    if(imgAtual === slider.length -1){
+        imgAtual = 0
+    }else{
+        imgAtual++
+    }
+    
+    
+    remover()
+    console.log(slider)
+}
+
+function anterior(){
+    addicionar()
+    if(imgAtual === 0){
+        imgAtual = slider.length -1
+    }else{
+        imgAtual--
+    }
+    remover()
+}
+
+prev.addEventListener('click', anterior)
+next.addEventListener('click', prox)
+
+

20432bc053105631401380b96ba04f60db20b0a8
Joel Ribeiro <joelribeirod2201@gmail.com>
2024-09-20 15:33:25 -0400

    paginas de produtos pronto

--- Tentativas/Tentativa05/paginassecundarias/paginasprodutos/script_produtos.js
+++ Tentativas/Tentativa05/paginassecundarias/paginasprodutos/script_produtos.js
@@ -1,25 +1,27 @@
-//teste criar um if else do seguinte modo
-//se a tela for para pc, deixa mostrando imagens, esconder as outras duas, e por fim fazer o carrossel
-//se a tela for para celular, deixar mostrando apenas uma imagem, esconder o resto, e deixar o carrossel rodando
-//Só n sei como vou fazer para ter o efeito de rolagem lateral, tipo, vc ver o carrosel indo pro lado e escondendo a imagem
+const slider = document.querySelectorAll('.carrosel-item')
+const sliderCel = document.querySelectorAll('.carrosel')
 
-const slider = document.querySelectorAll('.imgcarro')
-
+const carrosselItems = document.querySelectorAll('.carrosel')
 
 let intervalo
+let outrointervalo
 let imgAtual = 0
 
+//Celular
 function addicionar(){
-    slider.forEach(item => item.classList.add('off'))
+    sliderCel.forEach(item => item.classList.add('off'))
 }
 
 function remover(){
-    slider[imgAtual].classList.remove('off')
+    sliderCel[imgAtual].classList.remove('off')
 }
 
 function prox(){
     addicionar()
-    if(imgAtual === slider.length -1){
+    for(let i = 0; i<5; i++){
+        carrosselItems[i].style.transform = `translateX(${0}px)`
+    }
+    if(imgAtual === sliderCel.length -1){
         imgAtual = 0
     }else{
         imgAtual++
@@ -30,10 +32,42 @@ function prox(){
 function carroCel(){
     clearInterval(intervalo)
     intervalo = setInterval(prox, 2000)
+    clearInterval(outrointervalo)
+}
+//fimCelular
+
+//Pc
+function mover(){
+    let tamanhoimg = document.querySelector('.teste').clientWidth
+    let deslocamento = -imgAtual * tamanhoimg
+    for(let i = 0; i<5; i++){
+        carrosselItems[i].style.transform = `translateX(${deslocamento}px)`
+    }
 }
 
+function proxPc(){
+    if(imgAtual < carrosselItems.length - 3){
+        imgAtual++
+    }else{
+        imgAtual = 0
+    }
+    mover()
+}
+
+function carroPc(){
+    clearInterval(outrointervalo)
+    outrointervalo = setInterval(proxPc, 2000)
+    clearInterval(intervalo)
+}
+//fimPc
+
 function inicializar(){
-    carroCel()
+    if(window.innerWidth < 768){
+        carroCel()
+    }else if(window.innerWidth >= 768){
+        carroPc()
+    }
+    
 }
 
 inicializar()

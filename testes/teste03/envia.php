d1a0c54f26788a091e465c09cfcfa4b010995cc5
Joel Ribeiro <joelribeirod2201@gmail.com>
2024-09-27 19:57:16 -0400

    algumas correções e um teste

---
+++ testes/teste03/envia.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste</title>
+</head>
+<body>
+    <?php
+    $conexao = mysqli_connect("localhost", "root", "#Joel2210", "teste");
+    if(!$conexao){
+        echo"Não conectado";
+    }
+    echo"Conectado ao Banco>>>>>>>";
+
+    $cpf = $_POST['cpf'];
+    $cpf = mysqli_real_escape_string($conexao, $cpf);
+    $sql = "SELECT cpf FROM teste.dados WHERE cpf='$cpf'";
+    $retorno = mysqli_query($conexao, $sql);
+
+    if(mysqli_num_rows($retorno)>0){
+        echo"CPF ja cadastrado!<br>";
+    }else{
+        $cpf = $_POST['cpf'];
+        $idade = $_POST['idade'];
+        $nome = $_POST['nome'];
+
+        $sql = "INSERT INTO teste.dados(cpf, idade, nome) values('$cpf', '$idade', '$nome')";
+        $resultado = mysqli_query($conexao, $sql);
+        echo">> Usuário cadastrado com sucesso!<br>";
+    }
+    ?>
+</body>
+</html>

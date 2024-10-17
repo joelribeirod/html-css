8928f46f49c3a5001eda347fff8869c13c65ea10
Joel Ribeiro <joelribeirod2201@gmail.com>
2024-10-17 07:55:59 -0400

    inicio da T6, flashcards

---
+++ Tentativas/tentativa06/ambiente.js
@@ -0,0 +1,9 @@
+const adicionarCard = document.getElementById('config-tot')
+
+function mostrar(){
+    if(adicionarCard.style.display == 'none'){
+        adicionarCard.style.display = 'block'
+    }else{
+        adicionarCard.style.display = 'none'
+    }
+}

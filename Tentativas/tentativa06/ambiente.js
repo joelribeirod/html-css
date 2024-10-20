87c60029302b9778952ba43cdb39c1b9008cdcc3
Joel Ribeiro <joelribeirod2201@gmail.com>
2024-10-20 10:20:07 -0400

    projeto finalizado
    só faltou modificar para a seta girar quando abrir a descrição, mas n consegui fazer agora, talvez depois eu continue tentando

--- Tentativas/tentativa06/ambiente.js
+++ Tentativas/tentativa06/ambiente.js
@@ -54,38 +54,45 @@ enviar.addEventListener('click', ()=>{
     let configCard = document.createElement('div')
     configCard.className = 'configCard'
     
-    let setinha = document.createElement('span')
-    setinha.className = 'material-symbols-outlined'
+    var setinha = document.createElement('span')
+    setinha.className = 'material-symbols-outlined setinha'
     setinha.textContent = 'arrow_drop_down'
 
     let config = document.createElement('div')
     config.className = 'config'
 
     let settings = document.createElement('span')
-    settings.className = 'material-symbols-outlined'
+    settings.className = 'material-symbols-outlined spanconfig'
     settings.textContent = 'settings'
 
     let deletar = document.createElement('span')
     deletar.className = 'material-symbols-outlined deletar'
     deletar.textContent = 'delete'
 
+    //edita um card ja existente
     settings.addEventListener('click', () => {
+        editR.value = ''
+        editC.value = ''
         edit.style.display = 'flex'
         edit.addEventListener('click', (clicou) => {
             if(clicou.target.value == 1){
                 edit.style.display = 'none'
             }else if(clicou.target.value == 2){
+                //pega os valores que foram digitados na edit
                 testeR = document.getElementById('editResumo').value
                 testeC = document.getElementById('editDesc').value
-                
-                resumo.textContent = ''
-                total.textContent = ''
-
-                resumo.appendChild(setinha)
-                resumo.innerHTML += ` ${testeR}`
-                total.textContent = testeC
-
-                console.log(testeC, testeR)
+ 
+                //apaga o valor anterior e coloca os novos e faz a comparação, se o valor for nulo, ou seja, se a caixa n for alterada, o valor antigo permanece
+                if(testeR != ''){
+                    resumo.textContent = ''
+                    resumo.appendChild(setinha)
+                    resumo.innerHTML += ` ${testeR}`
+                }
+                if(testeC != ''){
+                    total.textContent = ''
+                    total.textContent = testeC
+                }
+                //faz a edição sumir
                 edit.style.display = 'none'
             }
         })
@@ -100,16 +107,20 @@ enviar.addEventListener('click', ()=>{
     total.className = 'total'
     total.textContent = desc
 
+    //faz com que a descricao apareça quando clicar no resumo
     configCard.addEventListener('click', (event) => {
         if(event.target === configCard || event.target.closest(".resumo")){
             if(total.style.display == 'block'){
-                total.style.display = 'none'
+                total.style.display = 'none';
+                //nao esta funcionando -> setinha.classList.remove('setinha');
             }else{
-                total.style.display = 'block'
+                total.style.display = 'block';
+                //nao esta funcionando -> setinha.classList.add('setinha');
             }
         }
     })
 
+    //remove o card que foi clicado
     deletar.addEventListener('click', () => {
         confirmDel.style.display = 'flex'
         confirmDel.addEventListener('click', (clicou) => {
